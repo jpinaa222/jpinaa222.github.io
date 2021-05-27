7eda7ba5c57e47da15d92589b0fa47005b3f3b1a
jpinaa222 <84928712+jpinaa222@users.noreply.github.com>
2021-05-27 12:26:08 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,1 +1,12 @@
-
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyCOC6Lv6NJjGnWL_cgjiUV_AyOCclUPFac",
+    authDomain: "jpinaa222-ef98a.firebaseapp.com",
+    projectId: "jpinaa222-ef98a",
+    storageBucket: "jpinaa222-ef98a.appspot.com",
+    messagingSenderId: "483386836015",
+    appId: "1:483386836015:web:2ec946055e1cb70739b2da",
+    measurementId: "G-WPD8D0E8RF"
+  });
